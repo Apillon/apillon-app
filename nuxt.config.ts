cf5eb2e4a9f70c704286211c1ff91cf27c0325ea
Simon Šenk <simon.senk@kalmia.si>
2023-03-04 22:38:22 +0100

    remove naive

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,7 +41,22 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
     'nuxt-icons',
-    // '@nuxtjs/google-fonts',
+    [
+      '@nuxtjs/google-fonts',
+      {
+        useStylesheet: true,
+        display: 'swap',
+        download: false,
+        families: {
+          'IBM Plex Mono': {
+            wght: [400, 700],
+          },
+          'IBM Plex Sans': {
+            wght: [400, 700],
+          },
+        },
+      },
+    ],
     '@nuxtjs/i18n',
   ],
 
@@ -120,20 +135,6 @@ export default defineNuxtConfig({
       ],
     },
   },
-
-  // googleFonts: {
-  //   useStylesheet: true,
-  //   display: 'swap',
-  //   download: false,
-  //   families: {
-  //     'IBM Plex Mono': {
-  //       wght: [400, 700],
-  //     },
-  //     'IBM Plex Sans': {
-  //       wght: [400, 700],
-  //     },
-  //   },
-  // },
 
   i18n: {
     // add `vueI18n` option to `@nuxtjs/i18n` module options
