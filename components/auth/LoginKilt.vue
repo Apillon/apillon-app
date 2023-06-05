3d1d755059cff2a1bec1d0d4577cc8e27f90815d
Luka Golinar <luka.golinar@kalmia.si>
2023-06-05 13:20:36 +0200

    feat: Removed code from index, since it was moved to component.

--- components/auth/LoginKilt.vue
+++ components/auth/LoginKilt.vue
@@ -1,4 +1,4 @@
-<template>
+<template v-if="sessionToken !== ''">
   <Btn v-bind="$attrs" type="primary" @click="openPopup()">
     <div class="flex items-center">
       <NuxtIcon :name="`auth/kilt-logo`" class="text-xl align-sub mr-2" filled />
@@ -12,7 +12,6 @@
 const config = useRuntimeConfig();
 const authStore = useAuthStore();
 const dataStore = useDataStore();
-const loading = ref(false);
 const sessionToken = ref<string | undefined>('');
 const oauthAuthToken = ref<string | undefined>('');
 const childWindow = ref<(Window & typeof globalThis) | undefined>();
@@ -25,14 +24,9 @@ onMounted(async () => {
   handlerFunction.value = async event => {
     if (event.data.verified) {
       oauthAuthToken.value = event.data.data.userData;
-      if (loading.value !== true) {
-        // Lock logging process just in case
-        loading.value = true;
-        await loginWithKilt();
-        loading.value = false;
-
-        childWindow.value?.close();
-      }
+      // Close before proceeding
+      childWindow.value?.close();
+      await loginWithKilt();
     }
   };
   window.addEventListener('message', handlerFunction.value, false);
@@ -61,11 +55,8 @@ async function loginWithKilt() {
 }
 
 async function getOauthSession() {
-  loading.value = true;
-
   try {
     const response = await $api.get<OauthSessionResponse>(endpoints.oauthSession);
-    loading.value = false;
     return response.data.data.session;
   } catch (error) {
     console.error(error);
