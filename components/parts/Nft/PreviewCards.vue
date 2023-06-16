702353b20d7c3aa34011e358904b270e7b9a83f4
Urban Kovač <urban.kovac@kalmia.si>
2023-06-16 14:10:40 +0200

    Swiper NFT card (swipe with keyboard)

--- components/parts/Nft/PreviewCards.vue
+++ components/parts/Nft/PreviewCards.vue
@@ -6,6 +6,7 @@
       :grab-cursor="true"
       :pagination="pagination"
       :modules="[EffectCards, Pagination]"
+      @swiper="onSwiper"
       @slideChange="onSlideChange"
     >
       <swiper-slide v-for="(slide, key) in slides" :key="key" class="justify-center flex mx-auto">
@@ -37,6 +38,7 @@ import 'swiper/css/navigation';
 const collectionStore = useCollectionStore();
 
 const SLIDES_TO_SHOW = 10;
+const swiperRef = ref<SwiperClass>();
 const slides = ref<Array<any>>([]);
 
 const pagination = reactive({
@@ -49,6 +51,30 @@ const pagination = reactive({
 onMounted(() => {
   slides.value = collectionStore.csvData.slice(0, SLIDES_TO_SHOW);
 });
+onUnmounted(() => {
+  if (swiperRef.value) {
+    window.removeEventListener('keydown', onKeyPressed);
+  }
+});
+
+const onSwiper = swiper => {
+  swiperRef.value = swiper;
+  useTimeoutFn(() => {
+    window.addEventListener('keydown', onKeyPressed);
+  }, 500);
+};
+
+function onKeyPressed(e: KeyboardEvent) {
+  if (swiperRef.value) {
+    switch (e.keyCode) {
+      case 37:
+        swiperRef.value.slidePrev();
+        break;
+      case 39:
+        swiperRef.value.slideNext();
+    }
+  }
+}
 
 function onSlideChange(swiper: SwiperClass) {
   if (
