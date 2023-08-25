89d767cd010ac8582125cc7b7e2af38dbcb6be4a
Urban Kovač <urban.kovac@kalmia.si>
2023-08-25 10:40:20 +0200

    NFT Collection (added collection address to header)

--- components/dashboard/header/Collection.vue
+++ components/dashboard/header/Collection.vue
@@ -7,18 +7,32 @@
         </NuxtLink>
         <h2>{{ collectionStore.active.name }}</h2>
 
-        <n-space class="mt-2" align="center" size="small" :wrap="false">
-          <span>{{ $t('nft.collection.uuid') }}:</span>
-          <n-ellipsis class="text-body align-bottom" :line-clamp="1">
-            {{ collectionStore.active.collection_uuid }}
-          </n-ellipsis>
-          <button
-            class="text-body"
-            @click="copyToClipboard(collectionStore.active.collection_uuid)"
-          >
-            <span class="icon-copy"></span>
-          </button>
-        </n-space>
+        <div>
+          <n-space class="" align="center" size="small" :wrap="false">
+            <span>{{ $t('nft.collection.address') }}:</span>
+            <n-ellipsis class="text-body align-bottom" :line-clamp="1">
+              {{ collectionStore.active.contractAddress }}
+            </n-ellipsis>
+            <button
+              class="text-body"
+              @click="copyToClipboard(collectionStore.active.contractAddress || '')"
+            >
+              <span class="icon-copy"></span>
+            </button>
+          </n-space>
+          <n-space class="" align="center" size="small" :wrap="false">
+            <span>{{ $t('nft.collection.uuid') }}:</span>
+            <n-ellipsis class="text-body align-bottom" :line-clamp="1">
+              {{ collectionStore.active.collection_uuid }}
+            </n-ellipsis>
+            <button
+              class="text-body"
+              @click="copyToClipboard(collectionStore.active.collection_uuid)"
+            >
+              <span class="icon-copy"></span>
+            </button>
+          </n-space>
+        </div>
       </n-space>
     </slot>
     <template #info>
