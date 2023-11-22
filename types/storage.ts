7995c73f0dda24329d9fe5af47ea8161f9ddcd65
Urban Kovač <urban.kovac@kalmia.si>
2023-11-22 13:35:19 +0100

    Hosting (added deployment statuses)

--- types/storage.ts
+++ types/storage.ts
@@ -75,8 +75,11 @@ export enum DeploymentEnvironment {
 export enum DeploymentStatus {
   INITIATED = 0,
   IN_PROGRESS = 1,
+  IN_REVIEW = 2,
+  APPROVED = 3,
   SUCCESSFUL = 10,
   FAILED = 100,
+  REJECTED = 101,
 }
 
 declare global {
