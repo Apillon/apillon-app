35acd0dffaa762ce1aee13b2c04665d76e935525
Urban Kovač <urban.kovac@kalmia.si>
2024-04-03 15:04:14 +0200

    NFT flow (added chain type)

--- components/dashboard/form/nft/Collection/EnvironmentSelect.vue
+++ components/dashboard/form/nft/Collection/EnvironmentSelect.vue
@@ -8,7 +8,7 @@
         :key="chain.value"
         class="border-2 p-4 rounded-md border-bg-lightest hover:cursor-pointer w-40 flex justify-center"
         :class="collectionStore.form.base.chain === chain.value ? 'border-yellow' : ''"
-        @click="collectionStore.form.base.chain = chain.value"
+        @click="onChainChange(chain.value)"
       >
         <div>
           <NuxtIcon
@@ -20,6 +20,32 @@
         </div>
       </div>
     </div>
+
+    <div
+      class="transition-all overflow-hidden"
+      :class="isOnlyEvmChain ? 'max-h-0' : 'max-h-[20rem]'"
+    >
+      <h4 class="relative top-2">{{ $t('nft.collection.chainType') }}</h4>
+      <div class="flex gap-4 whitespace-pre-line my-8 justify-center">
+        <div
+          v-for="chainType in chainTypes"
+          :key="chainType.value"
+          class="border-2 p-4 rounded-md border-bg-lightest hover:cursor-pointer w-40 flex justify-center"
+          :class="collectionStore.form.base.chainType === chainType.value ? 'border-yellow' : ''"
+          @click="collectionStore.form.base.chainType = chainType.value"
+        >
+          <div>
+            <NuxtIcon
+              :name="`logo/${chainType.name}`"
+              class="flex justify-center mx-auto text-7xl"
+              filled
+            />
+            <p>{{ chainType.label }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <Btn @click="handleSubmit()">{{ $t('form.proceed') }}</Btn>
   </div>
 </template>
@@ -31,6 +57,24 @@ const collectionStore = useCollectionStore();
 const chains = enumKeys(Chains).map(k => {
   return { name: k.toLowerCase(), label: t(`nft.chain.${Chains[k]}`), value: Chains[k] };
 });
+
+const chainTypes = enumKeys(ChainType).map(k => {
+  return { name: k.toLowerCase(), label: t(`nft.chainType.${ChainType[k]}`), value: ChainType[k] };
+});
+
+const isOnlyEvmChain = computed(
+  () =>
+    collectionStore.form.base.chain === Chains.MOONBASE ||
+    collectionStore.form.base.chain === Chains.MOONBEAM
+);
+
+function onChainChange(chain: number) {
+  collectionStore.form.base.chain = chain;
+
+  if (chain === Chains.MOONBASE || chain === Chains.MOONBEAM) {
+    collectionStore.form.base.chainType = ChainType.EVM;
+  }
+}
 
 function handleSubmit() {
   collectionStore.step = CollectionStep.METADATA;
