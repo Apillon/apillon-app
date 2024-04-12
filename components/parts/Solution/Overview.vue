6d6960e85b46ea2c2bb5f2c1f5885436d0008186
Damjan Dimitrov <damjan.dimitrov@apillon.io>
2024-04-12 12:56:25 +0200

    Rename project overview property

--- components/parts/Solution/Overview.vue
+++ components/parts/Solution/Overview.vue
@@ -97,8 +97,8 @@ const web3Services = computed(() => [
     icon: 'icon-nft',
     data: [
       {
-        name: t(`dashboard.service.nft.numOfCollections`),
-        value: dataStore.project.overview.numOfCollections,
+        name: t(`dashboard.service.nft.collectionCount`),
+        value: dataStore.project.overview.collectionCount,
       },
       {
         name: t(`dashboard.service.nft.nftTransactionCount`),
