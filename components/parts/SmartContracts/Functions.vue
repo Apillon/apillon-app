06dcf0dcd8c41b86fd4767b20a7801c3fb87c2d5
Martin Pislar <pislarm@gmail.com>
2024-07-15 14:21:17 +0200

    functions

--- components/parts/SmartContracts/Functions.vue
+++ components/parts/SmartContracts/Functions.vue
@@ -1,9 +1,14 @@
 <template>
-  <h1>functions {{ props.name }}</h1>
+  <div class="flex gap-2 mt-6">
+    <div class="relative card-light border-none p-4 w-1/5">
+      <div>functions list</div>
+    </div>
+    <div class="w-4/5">Other props</div>
+  </div>
 </template>
 
 <script setup lang="ts">
-const props = defineProps({
-  name: { type: String, default: '' },
-});
+// const props = defineProps({
+//   name: { type: String, default: '' },
+// });
 </script>
