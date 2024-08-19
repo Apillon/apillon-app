6f6fbdb01bd2876285b54e5df5bfc918bc8e6718
Martin Pislar <pislarm@gmail.com>
2024-08-19 08:31:41 +0200

    non / payable

--- pages/dashboard/service/smart-contracts/deployed/[id].vue
+++ pages/dashboard/service/smart-contracts/deployed/[id].vue
@@ -7,65 +7,120 @@
     </template>
 
     <slot>
-      <template v-for="fn in functionObjects" :key="fn">
-        <!-- If function has any availablbe inouts create a form -->
-        <template
-          v-if="
-            fn.inputs.length &&
-            (fn.stateMutability === 'payable' || fn.stateMutability === 'nonpayable')
-          "
-        >
-          {{ fn.stateMutability }}
-          <n-card size="small" class="my-1 max-w-lg">
-            <n-collapse accordion>
-              <n-collapse-item :title="fn.name">
-                <!-- Assign a fromref according to function ref - we have multiple form on same site -->
-                <n-form
-                  :ref="el => (formRefs[fn.name] = el)"
-                  :model="form[fn.name]"
-                  class="max-w-lg"
-                  @submit.prevent="handleSubmit"
-                >
-                  <!-- <h3>{{ fn.stateMutability }}</h3> -->
-                  {{ fn }}
-                  <!-- Create inouts -->
-                  <template v-for="i in fn.inputs" :key="i.name">
-                    <n-form-item :label="i.name">
-                      <n-input
-                        v-model:value="form[fn.name][i.name]"
-                        :maxlength="256"
-                        required
-                        :class="{
-                          'error-input': formErrors[fn.name],
-                        }"
-                      />
-                    </n-form-item>
-                  </template>
-                  <!-- Submit -->
-                  <n-button type="primary" native-type="submit" @click="handleSubmit(fn.name)">
-                    Query
-                  </n-button>
-                  <!-- Error container for each form -->
-                  <Notification v-if="errors[fn.name]" type="error" class="mt-6">
-                    Something went wrong, please try again or try later.
-                  </Notification>
-                </n-form>
-              </n-collapse-item>
-            </n-collapse>
-          </n-card>
-        </template>
-        <template v-else>
-          <!-- If function doesnt have inputs avialble, just output function data -->
-          <n-card size="small" class="my-1 max-w-lg">
-            <n-collapse accordion>
-              <n-collapse-item :title="fn.name">
-                {{ fn.outputs[0]?.internalType }}
-              </n-collapse-item>
-            </n-collapse>
-          </n-card>
-        </template>
-      </template>
-      <!-- <pre>{{ functionObjects }}</pre> -->
+      <div class="flex">
+        <!-- wrtie functions -->
+        <div class="p-2 w-full max-w-lg">
+          <template v-for="fn in writeFunctions" :key="fn">
+            <!-- If function has any availablbe inouts create a form -->
+            <template v-if="fn.inputs.length">
+              <n-card size="small" class="my-1 max-w-lg">
+                <n-collapse accordion>
+                  <n-collapse-item :title="fn.name">
+                    <!-- Assign a fromref according to function ref - we have multiple form on same site -->
+                    <template #header-extra>
+                      <n-tag size="small" type="warning" round>
+                        <span class="text-orange"> {{ fn.onlyOwner }}</span>
+                      </n-tag>
+                    </template>
+
+                    <n-form
+                      :ref="el => (formRefs[fn.name] = el)"
+                      :model="form[fn.name]"
+                      class="max-w-lg"
+                      @submit.prevent="handleSubmit"
+                    >
+                      <!-- Create inouts -->
+                      <template v-for="i in fn.inputs" :key="i.name">
+                        <n-form-item :label="i.name">
+                          <n-input
+                            v-model:value="form[fn.name][i.name]"
+                            :maxlength="256"
+                            required
+                            :class="{
+                              'error-input': formErrors[fn.name],
+                            }"
+                          />
+                        </n-form-item>
+                      </template>
+                      <!-- Submit -->
+                      <n-button type="primary" native-type="submit" @click="handleSubmit(fn.name)">
+                        Query
+                      </n-button>
+                      <!-- Error container for each form -->
+                      <Notification v-if="errors[fn.name]" type="error" class="mt-6">
+                        Something went wrong, please try again or try later.
+                      </Notification>
+                    </n-form>
+                  </n-collapse-item>
+                </n-collapse>
+              </n-card>
+            </template>
+            <template v-else>
+              <!-- If function doesnt have inputs avialble, just output function data -->
+              <n-card size="small" class="my-1 max-w-lg">
+                <n-collapse accordion>
+                  <n-collapse-item :title="fn.name">
+                    {{ fn.outputs[0]?.internalType }}
+                  </n-collapse-item>
+                </n-collapse>
+              </n-card>
+            </template>
+          </template>
+        </div>
+        <!-- read functions -->
+        <div class="bg-black p-2 w-full max-w-lg rounded-lg">
+          <template v-for="fn in readFunctions" :key="fn">
+            <!-- If function has any availablbe inouts create a form -->
+            <template v-if="fn.inputs.length">
+              <n-card size="small" class="my-1 max-w-lg">
+                <n-collapse accordion>
+                  <n-collapse-item :title="fn.name">
+                    <!-- Assign a fromref according to function ref - we have multiple form on same site -->
+                    <n-form
+                      :ref="el => (formRefs[fn.name] = el)"
+                      :model="form[fn.name]"
+                      class="max-w-lg"
+                      @submit.prevent="handleSubmit"
+                    >
+                      <!-- Create inouts -->
+                      <template v-for="i in fn.inputs" :key="i.name">
+                        <n-form-item :label="i.name">
+                          <n-input
+                            v-model:value="form[fn.name][i.name]"
+                            :maxlength="256"
+                            required
+                            :class="{
+                              'error-input': formErrors[fn.name],
+                            }"
+                          />
+                        </n-form-item>
+                      </template>
+                      <!-- Submit -->
+                      <n-button type="primary" native-type="submit" @click="handleSubmit(fn.name)">
+                        Query
+                      </n-button>
+                      <!-- Error container for each form -->
+                      <Notification v-if="errors[fn.name]" type="error" class="mt-6">
+                        Something went wrong, please try again or try later.
+                      </Notification>
+                    </n-form>
+                  </n-collapse-item>
+                </n-collapse>
+              </n-card>
+            </template>
+            <template v-else>
+              <!-- If function doesnt have inputs avialble, just output function data -->
+              <n-card size="small" class="my-1 max-w-lg">
+                <n-collapse accordion>
+                  <n-collapse-item :title="fn.name">
+                    {{ fn.outputs[0]?.internalType }}
+                  </n-collapse-item>
+                </n-collapse>
+              </n-card>
+            </template>
+          </template>
+        </div>
+      </div>
     </slot>
   </Dashboard>
 </template>
@@ -87,6 +142,8 @@ const contractUuid = ref<string>(`${params?.id}` || '');
 
 // Data
 const functionObjects = ref([]);
+const writeFunctions = ref([]);
+const readFunctions = ref([]);
 
 const form = reactive({});
 const formRefs = ref({});
@@ -154,6 +211,20 @@ onMounted(() => {
         fn.inputs.forEach(input => {
           form[fn.name][input.name] = null; // Initialize each input field
         });
+
+        if (fn.stateMutability === 'nonpayable' || fn.stateMutability === 'payable') {
+          // Find the corresponding method in state.contractVersion.methods
+          const method = smartContractsStore.active.contractVersion.methods.find(
+            method => method.name === fn.name
+          );
+          // Add the method to writeFunctions and tag it with onlyOwner if applicable
+          writeFunctions.value.push({
+            ...fn, // Spread the function properties
+            onlyOwner: method ? method.onlyOwner : false, // Add onlyOwner property
+          });
+        } else {
+          readFunctions.value.push(fn);
+        }
       });
 
       pageLoading.value = false;
