f7fb1d2c847b23843cb0874472dbb3cc731321a6
Urban Kovač <urban.kovac@kalmia.si>
2024-10-11 09:57:08 +0200

    Asset hub (nav changes)

--- components/dashboard/menu/Nav.vue
+++ components/dashboard/menu/Nav.vue
@@ -24,7 +24,7 @@ const props = defineProps({
 });
 defineEmits(['toggleSidebar']);
 
-const $i18n = useI18n();
+const { t } = useI18n();
 const authStore = useAuthStore();
 const dataStore = useDataStore();
 const { name } = useRoute();
@@ -41,7 +41,7 @@ const defaultExpandedKeys = computed(() =>
 const menuOptions = computed<MenuMixedOption[]>(() => {
   const dashboard = {
     key: 'dashboard',
-    label: $i18n.t('dashboard.nav.projectOverview'),
+    label: t('dashboard.nav.projectOverview'),
     to: 'dashboard',
     iconName: 'icon-home',
     disabled: isMenuItemDisabled(Feature.PROJECT),
@@ -50,7 +50,7 @@ const menuOptions = computed<MenuMixedOption[]>(() => {
   const servicesChildren = [
     {
       key: 'dashboard-service',
-      label: $i18n.t('dashboard.nav.explore'),
+      label: t('dashboard.nav.explore'),
       to: 'dashboard-service',
       class: 'text-yellow',
       iconName: 'icon-wide-right',
@@ -58,7 +58,7 @@ const menuOptions = computed<MenuMixedOption[]>(() => {
     },
     {
       key: 'dashboard-service-storage',
-      label: $i18n.t('dashboard.nav.storage'),
+      label: t('dashboard.nav.storage'),
       to: 'dashboard-service-storage',
       iconName: 'icon-storage',
       soon: isMenuItemDisabled(Feature.STORAGE),
@@ -69,7 +69,7 @@ const menuOptions = computed<MenuMixedOption[]>(() => {
     },
     {
       key: 'dashboard-service-hosting',
-      label: $i18n.t('dashboard.nav.hosting'),
+      label: t('dashboard.nav.hosting'),
       to: 'dashboard-service-hosting',
       iconName: 'icon-hosting',
       soon: isMenuItemDisabled(Feature.HOSTING),
@@ -80,7 +80,7 @@ const menuOptions = computed<MenuMixedOption[]>(() => {
     },
     {
       key: 'dashboard-service-nft',
-      label: $i18n.t('dashboard.nav.nft'),
+      label: t('dashboard.nav.nft'),
       to: 'dashboard-service-nft',
       iconName: 'icon-NFTs',
       soon: isMenuItemDisabled(Feature.NFT),
@@ -91,7 +91,7 @@ const menuOptions = computed<MenuMixedOption[]>(() => {
     },
     {
       key: 'dashboard-service-authentication',
-      label: $i18n.t('dashboard.nav.authentication'),
+      label: t('dashboard.nav.authentication'),
       to: 'dashboard-service-authentication',
       iconName: 'icon-authentication',
       soon: isMenuItemDisabled(Feature.AUTHENTICATION),
@@ -101,8 +101,18 @@ const menuOptions = computed<MenuMixedOption[]>(() => {
         zeroProjects.value,
     },
     {
+      key: 'dashboard-service-asset-hub',
+      label: t('dashboard.nav.assetHub'),
+      to: 'dashboard-service-asset-hub',
+      iconName: 'icon-social',
+      disabled:
+        isMenuItemDisabled(Feature.ASSET_HUB) ||
+        !authStore.isUserAllowed(Permission.EMBEDDED_WALLET) ||
+        zeroProjects.value,
+    },
+    {
       key: 'dashboard-service-computing',
-      label: $i18n.t('dashboard.nav.computing'),
+      label: t('dashboard.nav.computing'),
       to: 'dashboard-service-computing',
       iconName: 'icon-computing',
       soon: isMenuItemDisabled(Feature.COMPUTING),
@@ -112,18 +122,8 @@ const menuOptions = computed<MenuMixedOption[]>(() => {
         zeroProjects.value,
     },
     {
-      key: 'dashboard-service-asset-hub',
-      label: $i18n.t('dashboard.nav.assetHub'),
-      to: 'dashboard-service-asset-hub',
-      iconName: 'icon-social',
-      disabled:
-        isMenuItemDisabled(Feature.ASSET_HUB) ||
-        !authStore.isUserAllowed(Permission.EMBEDDED_WALLET) ||
-        zeroProjects.value,
-    },
-    {
       key: 'dashboard-service-social',
-      label: $i18n.t('dashboard.nav.social'),
+      label: t('dashboard.nav.social'),
       to: 'dashboard-service-social',
       iconName: 'icon-social',
       soon: isMenuItemDisabled(Feature.SOCIAL),
@@ -137,7 +137,7 @@ const menuOptions = computed<MenuMixedOption[]>(() => {
   const smartContractsChildren = [
     {
       key: 'dashboard-smart-contracts',
-      label: $i18n.t('dashboard.nav.explore'),
+      label: t('dashboard.nav.explore'),
       class: 'text-yellow',
       iconName: 'icon-wide-right',
       show: !props.collapsed,
@@ -172,7 +172,7 @@ const menuOptions = computed<MenuMixedOption[]>(() => {
     ? [
         {
           key: 'dashboard-solution',
-          label: $i18n.t('dashboard.nav.explore'),
+          label: t('dashboard.nav.explore'),
           to: 'dashboard-solution',
           class: 'text-yellow',
           iconName: 'icon-wide-right',
@@ -180,35 +180,35 @@ const menuOptions = computed<MenuMixedOption[]>(() => {
         },
         {
           key: 'dashboard-solution-airdrop',
-          label: $i18n.t('dashboard.solution.nftAirdrop.name'),
+          label: t('dashboard.solution.nftAirdrop.name'),
           iconName: 'icon-nft-mint-airdrop',
           to: 'dashboard-solution-airdrop',
           disabled: zeroProjects.value,
         },
         {
           key: 'dashboard-solution-email-signup-airdrop',
-          label: $i18n.t('dashboard.solution.nftEmailSignupAirdrop.name'),
+          label: t('dashboard.solution.nftEmailSignupAirdrop.name'),
           iconName: 'icon-nft-drop',
           to: 'dashboard-solution-email-signup-airdrop',
           disabled: zeroProjects.value,
         },
         {
           key: 'dashboard-solution-proof-of-attendance',
-          label: $i18n.t('dashboard.solution.nftPoap.name'),
+          label: t('dashboard.solution.nftPoap.name'),
           iconName: 'icon-poap',
           to: 'dashboard-solution-proof-of-attendance',
           disabled: zeroProjects.value,
         },
         {
           key: 'dashboard-solution-whitelist-claim',
-          label: $i18n.t('dashboard.solution.nftWhitelistClaim.name'),
+          label: t('dashboard.solution.nftWhitelistClaim.name'),
           iconName: 'icon-gift',
           to: 'dashboard-solution-whitelist-claim',
           disabled: zeroProjects.value,
         },
         {
           key: 'dashboard-solution-openGov',
-          label: $i18n.t('dashboard.solution.openGov.name'),
+          label: t('dashboard.solution.openGov.name'),
           iconName: 'icon-brand-membership',
           to: 'dashboard-solution-openGov',
           disabled: zeroProjects.value,
@@ -219,21 +219,21 @@ const menuOptions = computed<MenuMixedOption[]>(() => {
   const configurationChildren = [
     {
       key: 'dashboard-project-settings',
-      label: $i18n.t('dashboard.nav.projectSettings'),
+      label: t('dashboard.nav.projectSettings'),
       to: 'dashboard-project-settings',
       iconName: 'icon-project-setting',
       disabled: isMenuItemDisabled(Feature.PROJECT_SETTINGS) || zeroProjects.value,
     },
     {
       key: 'dashboard-access',
-      label: $i18n.t('dashboard.nav.access'),
+      label: t('dashboard.nav.access'),
       to: 'dashboard-users-permissions',
       iconName: 'icon-acess',
       disabled: isMenuItemDisabled(Feature.ACCESS) || zeroProjects.value,
     },
     {
       key: 'dashboard-api-keys',
-      label: $i18n.t('dashboard.nav.apiKeys'),
+      label: t('dashboard.nav.apiKeys'),
       to: 'dashboard-api-keys',
       iconName: 'icon-api-keys',
       disabled: isMenuItemDisabled(Feature.API_KEYS) || zeroProjects.value,
@@ -241,7 +241,7 @@ const menuOptions = computed<MenuMixedOption[]>(() => {
     },
     {
       key: 'dashboard-payments',
-      label: $i18n.t('dashboard.nav.billing'),
+      label: t('dashboard.nav.billing'),
       to: 'dashboard-payments',
       iconName: 'icon-billing',
       disabled: isMenuItemDisabled(Feature.BILLING) || zeroProjects.value,
@@ -252,7 +252,7 @@ const menuOptions = computed<MenuMixedOption[]>(() => {
   const solutionsMenu = isFeatureEnabled(Feature.PREBUILD_SOLUTIONS, authStore.getUserRoles())
     ? [
         {
-          label: $i18n.t('dashboard.nav.solutions'),
+          label: t('dashboard.nav.solutions'),
           key: 'solutions',
           children: [...solutionsChildren],
         },
@@ -291,19 +291,19 @@ const menuOptions = computed<MenuMixedOption[]>(() => {
     : [
         dashboard,
         {
-          label: $i18n.t('dashboard.nav.services'),
+          label: t('dashboard.nav.services'),
           key: 'services',
           show: !props.collapsed,
           children: [...servicesChildren],
         },
         // {
-        //   label: $i18n.t('dashboard.nav.smartContracts'),
+        //   label: t('dashboard.nav.smartContracts'),
         //   key: 'smart-contracts',
         //   children: [...smartContractsChildren],
         // },
         ...solutionsMenu,
         {
-          label: $i18n.t('dashboard.nav.configuration'),
+          label: t('dashboard.nav.configuration'),
           key: 'configuration',
           children: [...configurationChildren],
         },
