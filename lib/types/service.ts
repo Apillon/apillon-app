bfdeb6a01bf17227993b541be88361aa83d119f6
Urban Kovač <urban.kovac@kalmia.si>
2024-10-14 14:06:51 +0200

    Smart contracts (UI improvements)

--- lib/types/service.ts
+++ lib/types/service.ts
@@ -15,8 +15,8 @@ export enum ServiceTypeName {
   AUTHENTICATION = 'IDENTITY',
   NFT = 'NFT',
   COMPUTING = 'COMPUTING',
-  SOCIAL = 'SOCIAL',
   CONTRACTS = 'CONTRACTS',
+  SOCIAL = 'SOCIAL',
 }
 
 export enum IdentityChains {
