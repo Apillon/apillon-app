8c18fda1efdff2695024c288cd2002c4405b3038
Jaka Purg <purg.jaka@gmail.com>
2024-10-30 09:40:44 +0100

    Added RPC information to active RPC dashboard

--- components/parts/Rpc/Disabled.vue
+++ components/parts/Rpc/Disabled.vue
@@ -11,35 +11,7 @@
     <slot>
       <div class="flex flex-col h-full pb-8">
         <div class="flex flex-col flex-grow">
-          <div class="flex flex-col gap-2">
-            <h4>
-              {{ $t('rpc.apiKey.headline') }}
-            </h4>
-            <p>
-              {{ $t('rpc.apiKey.description') }}
-            </p>
-            <div class="text-body flex flex-row gap-4">
-              {{ $t('rpc.apiKey.powered') }}
-              <img
-                src="/assets/images/rpc/Dwellir.png"
-                class="h-6 w-auto object-contain"
-                alt="Dwellir"
-              />
-            </div>
-          </div>
-
-          <ul class="mt-6 flex-grow overflow-y-auto">
-            <li v-for="(benefit, keyB) in benefits" :key="keyB" class="my-2">
-              <NuxtIcon
-                name="icon/success"
-                class="inline-block float-left mr-2 text-2xl text-green"
-              />
-              <span>
-                <strong v-if="benefit.bolded">{{ benefit.bolded }}</strong>
-                {{ benefit.text }}
-              </span>
-            </li>
-          </ul>
+          <RpcFeatures />
           <div class="flex flex-col gap-8 border-b border-bg-lighter pb-8 my-8">
             <p class="text-sm font-bold text-white">{{ $t('rpc.apiKey.start') }}</p>
 
@@ -75,24 +47,11 @@
   </Dashboard>
 </template>
 <script lang="ts" setup>
-const { t, tm, rt } = useI18n();
+const { t } = useI18n();
 const router = useRouter();
 const rpcEndpointStore = useRpcEndpointStore();
 const dataStore = useDataStore();
 const pageLoading = ref<boolean>(true);
-
-const benefits = computed(() => {
-  const rawBenefits =
-    (tm('rpc.initial.benefits') as {
-      text?: string;
-      bolded?: string;
-    }[]) || [];
-
-  return rawBenefits.map(benefit => ({
-    text: benefit.text ? rt(benefit.text) : undefined,
-    bolded: benefit.bolded ? rt(benefit.bolded) : undefined,
-  }));
-});
 
 const onServiceCreated = () => {
   router.push('/dashboard/service/rpc/subscription');
