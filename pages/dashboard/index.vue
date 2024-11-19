1e6d9ac880c3288373a90001a39cc4871dc85561
Jaka Purg <purg.jaka@gmail.com>
2024-11-19 10:58:59 +0100

    Added wallet to form

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -6,7 +6,10 @@
       </Heading>
     </template>
     <slot>
-      <ProductHuntBanner v-if="authStore.user.wallet" />
+      <ProductHuntBanner
+        v-if="authStore.user.wallet"
+        :wallet="authStore.user.wallet ?? undefined"
+      />
       <div
         v-if="isFeatureEnabled(Feature.PREBUILD_SOLUTIONS, authStore.getUserRoles())"
         class="mb-8"
